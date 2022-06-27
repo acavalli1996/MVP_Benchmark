10e0a98bbf671bb454e64fe6e75c22e2b471adcc
acavalli1996 <53496133+acavalli1996@users.noreply.github.com>
2022-06-27 16:31:52 +0200

    Update pcn.py

--- completion/models/pcn.py
+++ completion/models/pcn.py
@@ -11,12 +11,12 @@ from model_utils import gen_grid_up, calc_emd, calc_cd
 
 
 class PCN_encoder(nn.Module):
-    def __init__(self, output_size=1024):
+    def __init__(self, output_size=75):
         super(PCN_encoder, self).__init__()
-        self.conv1 = nn.Conv1d(3, 128, 1)
-        self.conv2 = nn.Conv1d(128, 256, 1)
-        self.conv3 = nn.Conv1d(512, 512, 1)
-        self.conv4 = nn.Conv1d(512, output_size, 1)
+        self.conv1 = nn.Conv1d(4, 8, 1)
+        self.conv2 = nn.Conv1d(8, 16, 1)
+        self.conv3 = nn.Conv1d(32, 64, 1)
+        self.conv4 = nn.Conv1d(64, output_size, 1)
 
     def forward(self, x):
         batch_size, _, num_points = x.size()
@@ -35,28 +35,28 @@ class PCN_decoder(nn.Module):
         super(PCN_decoder, self).__init__()
         self.num_coarse = num_coarse
         self.num_fine = num_fine
-        self.fc1 = nn.Linear(1024, 1024)
-        self.fc2 = nn.Linear(1024, 1024)
-        self.fc3 = nn.Linear(1024, num_coarse * 3)
+        self.fc1 = nn.Linear(75, 75)
+        self.fc2 = nn.Linear(75, 75)
+        self.fc3 = nn.Linear(75, num_coarse * 4)
 
         self.scale = scale
         self.grid = gen_grid_up(2 ** (int(math.log2(scale))), 0.05).cuda().contiguous()
-        self.conv1 = nn.Conv1d(cat_feature_num, 512, 1)
-        self.conv2 = nn.Conv1d(512, 512, 1)
-        self.conv3 = nn.Conv1d(512, 3, 1)
+        self.conv1 = nn.Conv1d(cat_feature_num, 64, 1)
+        self.conv2 = nn.Conv1d(64, 64, 1)
+        self.conv3 = nn.Conv1d(64, 4, 1)
 
     def forward(self, x):
         batch_size = x.size()[0]
         coarse = F.relu(self.fc1(x))
         coarse = F.relu(self.fc2(coarse))
-        coarse = self.fc3(coarse).view(-1, 3, self.num_coarse)
+        coarse = self.fc3(coarse).view(-1, 4, self.num_coarse)
 
         grid = self.grid.clone().detach()
         grid_feat = grid.unsqueeze(0).repeat(batch_size, 1, self.num_coarse).contiguous().cuda()
 
         point_feat = (
             (coarse.transpose(1, 2).contiguous()).unsqueeze(2).repeat(1, 1, self.scale, 1).view(-1, self.num_fine,
-                                                                                                3)).transpose(1,
+                                                                                                4)).transpose(1,
                                                                                                               2).contiguous()
 
         global_feat = x.unsqueeze(2).repeat(1, 1, self.num_fine)
@@ -64,7 +64,7 @@ class PCN_decoder(nn.Module):
         feat = torch.cat((grid_feat, point_feat, global_feat), 1)
 
         center = ((coarse.transpose(1, 2).contiguous()).unsqueeze(2).repeat(1, 1, self.scale, 1).view(-1, self.num_fine,
-                                                                                                      3)).transpose(1,
+                                                                                                      4)).transpose(1,
                                                                                                                     2).contiguous()
 
         fine = self.conv3(F.relu(self.conv2(F.relu(self.conv1(feat))))) + center
@@ -72,7 +72,7 @@ class PCN_decoder(nn.Module):
 
 
 class Model(nn.Module):
-    def __init__(self, args, num_coarse=1024):
+    def __init__(self, args, num_coarse=75):
         super(Model, self).__init__()
 
         self.num_coarse = num_coarse
@@ -80,7 +80,7 @@ class Model(nn.Module):
         self.train_loss = args.loss
         self.eval_emd = args.eval_emd
         self.scale = self.num_points // num_coarse
-        self.cat_feature_num = 2 + 3 + 1024
+        self.cat_feature_num = 2 + 4 + 75
 
         self.encoder = PCN_encoder()
         self.decoder = PCN_decoder(num_coarse, self.num_points, self.scale, self.cat_feature_num)
