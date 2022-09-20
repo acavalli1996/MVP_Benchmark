4e850843369aefe088abde80230ae80de8ea39c2
acavalli1996 <53496133+acavalli1996@users.noreply.github.com>
2022-09-20 15:51:19 +0200

    Update test.py

--- completion/test.py
+++ completion/test.py
@@ -65,6 +65,101 @@ def test():
         _, _ = process.communicate()
         print("Submission file has been saved to %s/submission.zip" % (log_dir))
 
+        
+def make_plots(
+    losses,
+    epoch,
+    #real_jets,
+    gen_jets,
+    #real_mask,
+    gen_mask,
+    #jet_type,
+    num_particles,
+    name,
+    figs_path,
+    losses_path,
+    save_epochs=5,
+    const_ylim=False,
+    coords="polarrel",
+    dataset="jetnet",
+    loss="ls",
+):
+    """Plot histograms, jet images, loss curves, and evaluation curves"""
+    #real_masses = jetnet.utils.jet_features(real_jets)["mass"]
+    gen_masses = jetnet.utils.jet_features(gen_jets)["mass"]
+
+#    if "w1efp" in losses:
+#        real_efps = jetnet.utils.efps(real_jets)
+#        gen_efps = jetnet.utils.efps(gen_jets)
+#
+#        plotting.plot_part_feats(
+#            jet_type,
+#            real_jets,
+#            gen_jets,
+#            real_mask,
+#            gen_mask,
+#            name=name + "p",
+#            figs_path=figs_path,
+#            losses=losses,
+#            num_particles=num_particles,
+#            coords=coords,
+#            dataset=dataset,
+#            const_ylim=const_ylim,
+#            show=False,
+#        )
+#        plotting.plot_jet_feats(
+#            jet_type,
+#            real_masses,
+#            gen_masses,
+#            real_efps,
+#            gen_efps,
+#            name=name + "j",
+#            figs_path=figs_ph,
+#            losses=losses,
+#            show=False,
+#        )
+#   else:
+     plotting.plot_part_feats_jet_mass(
+          #jet_type,
+          #real_jets,
+          gen_jets,
+          #real_mask,
+          gen_mask,
+          #real_masses,
+          gen_masses,
+          name=None,
+          figs_path=None,
+          losses=None,
+          num_particles=150,
+          coords="polarrel",
+          dataset="jetnet",
+          const_ylim=False,
+          show=False,
+      )
+
+#    if len(losses["G"]) > 1:
+#        plotting.plot_losses(losses, loss=loss, name=name, losses_path=losses_path, show=False)
+
+#        try:
+#            remove(losses_path + "/" + str(epoch - save_epochs) + ".pdf")
+#        except:
+#            logging.info("Couldn't remove previous loss curves")
+
+#    if len(losses["w1p"]) > 1:
+#        plotting.plot_eval(
+#            losses,
+#            epoch,
+#            save_epochs,
+#            coords=coords,
+#            name=name + "_eval",
+#            losses_path=losses_path,
+#            show=False,
+#        )
+
+#        try:
+#            remove(losses_path + "/" + str(epoch - save_epochs) + "_eval.pdf")
+#        except:
+#            logging.info("Couldn't remove previous eval curves")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Test config file')
@@ -98,4 +193,4 @@ if __name__ == "__main__":
         const_ylim=args.const_ylim,
         coords=args.coords,
         loss=args.loss,
-     )
+    )
